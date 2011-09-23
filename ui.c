69a96b106b1a9933b4504caa8b2733c7ad7fe1d4
Scott <romracer@gmail.com>
2011-09-22 20:14:55 -0500

    Change our 'blue' color and keep the orange

--- ui.c
+++ ui.c
@@ -177,12 +177,12 @@ static void draw_text_line(int row, const char* t) {
 #elif BUILD_DARK_GREEN_COLOR
   #define MENU_TEXT_COLOR 0, 100, 0, 255
 #elif BUILD_BLUE_COLOR
-  #define MENU_TEXT_COLOR 0, 110, 255, 255
+//  #define MENU_TEXT_COLOR 0, 110, 255, 255
+  #define MENU_TEXT_COLOR 0, 191, 255, 255
 #elif BUILD_RED_COLOR
   #define MENU_TEXT_COLOR 211, 2, 2, 255
 #else
-//  #define MENU_TEXT_COLOR 255, 160, 49, 255
-  #define MENU_TEXT_COLOR 0, 191, 255, 255
+  #define MENU_TEXT_COLOR 255, 160, 49, 255
 #endif
 #define NORMAL_TEXT_COLOR 200, 200, 200, 255
 #define HEADER_TEXT_COLOR NORMAL_TEXT_COLOR
